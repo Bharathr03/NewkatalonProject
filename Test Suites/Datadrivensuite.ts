c6d1dadd54d0d823da86a1f3cbd0b6c59fb734ac
bharath <bharath@R3NRCX06F801136>
2024-03-13 15:42:07 +0530

    commit to push

---
+++ Test Suites/Datadrivensuite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Datadrivensuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9b53fac4-a7a8-4189-a64a-6ef9ca7cc60c</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ded8dd2-f3dc-41b3-af11-081e37e816a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Datadrivetestcase</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>374f6298-cba3-4f53-9648-80ed14ce9ed1</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Datadrivenexcel/TestDataexcel</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>374f6298-cba3-4f53-9648-80ed14ce9ed1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>7b20da49-c6fb-4f86-859a-6e58c645eba4</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>374f6298-cba3-4f53-9648-80ed14ce9ed1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>a71dfb9f-b8fd-4ac0-89c8-f730b5c6d979</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
